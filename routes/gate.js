0aafcf64a98dc996894f4a6b97d007bcb8fc1ba1
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2014-04-01 15:52:08 -0300

    Adding route /gates on gates service

--- routes/gate.js
+++ routes/gate.js
@@ -4,25 +4,35 @@
 
 
 module.exports = function (app) {
+
 	var gate = require('../models/gate.js');
 
+	function getGates(req, res, next){
+		'use static';
+
+//		req.params.date
+//		gate.find()
+
+
+
+	}
 
 	function addGate(req, res, next){
 		'use static';
 
 		var gate2insert = req.body;
 		if (gate2insert) {
-			gate.insert(gate2insert, function (err, data){
+			gate.insert(gate2insert, function (err, data) {
 				if (!err){
-					console.log('Gate inserted at %s', new Date().toString());
+					console.log('%s - Gate inserted.', moment().format('YYYY-MM-DD HH:MM'));
 					res.send(data);
 				} else {
 					res.send({"error": err});
 				}
-			})
+			});
 		}
 	}
 
-
 	app.post('/gate', addGate);
+	app.get('/gates/:date', getGates);
 }
