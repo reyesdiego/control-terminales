0f6610652569a25eccccdc82bc290c035e4ad63f
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2014-07-02 12:09:37 -0300

    Testing Cluster

--- err.js
+++ err.js
@@ -20,11 +20,52 @@ if (cluster.isMaster) {
 	});
 
 } else {
-
+	var express		=	require('express');
 	var http = require('http');
-	http.createServer(function (req, res) {
-		res.end("Look Mum! I'm a server!\n");
-	}).listen(3000, "127.0.0.1");
+
+	var app = express();
+	var server = http.createServer(app);
+
+	app.configure(function () {
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(app.router);
+//	app.use(passport.initialize());
+
+	});
+
+	app.all('/*', function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", 'X-Requested-With, Content-Type, token');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//	res.header('Access-Control-Request-Method', 'GET');
+		res.header('Access-Control-Request-Headers', 'Content-Type, token');
+
+		if ('OPTIONS' == req.method) {
+			res.send(200);
+		}
+		else {
+			next();
+		}
+//	next();
+	});
+
+	app.get('/g', function (req,res){
+		console.log(cluster.worker.id);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(cluster.worker.id.toString());
+		res.end();
+	});
+
+	server.listen(3000, function() {
+		console.log("===============================================================================");
+		console.log("- Nodejs server Version: %s", process.pid);
+	});
+
+//	http.createServer(function (req, res) {
+//		res.end("Look Mum! I'm a server!\n");
+//	}).listen(3000, "127.0.0.1");
+//	console.log('yo');
 
 }
 
