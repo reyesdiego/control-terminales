af404638ce70380bab42e026992c80263b8a449e
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2014-07-18 17:13:15 -0300

    Fixing bug in validation. The logged terminal has to be the same passed in the params.

--- routes/matchPrice.js
+++ routes/matchPrice.js
@@ -6,7 +6,7 @@
 module.exports = function (app){
 
 	var MatchPrice = require('../models/matchPrice.js');
-//	var matchPrice = require('../models/matchPrice.js');
+	var util = require('util');
 	var price = require('../models/price.js');
 	var dateTime = require('../include/moment');
 
@@ -21,86 +21,97 @@ module.exports = function (app){
 				console.error('%s - Error: %s', dateTime.getDatetime(), err);
 				res.send(500, {status:"ERROR", data:"Invalid or missing Token"});
 			} else {
+				var paramTerminal = req.params.terminal;
 
-				var ter = (usr.role === 'agp')?req.params.terminal:usr.terminal;
-				var param = {
-					$or : [
-						{terminal:	"AGP"},
-						{terminal:	ter}
-					]
-				};
+				if (usr.terminal !== 'AGP' && usr.terminal !== paramTerminal){
+					var errMsg = util.format('%s - Error: %s', dateTime.getDatetime(), 'La terminal recibida por parámetro es inválida para el token.');
+					console.error(errMsg);
+					res.send(500, {status:"ERROR", data: errMsg});
+				} else {
+					var ter = (usr.role === 'agp')?paramTerminal:usr.terminal;
+					var param = {
+						$or : [
+							{terminal:	"AGP"},
+							{terminal:	ter}
+						]
+					};
 
-				if (req.query.code){
-					param.code = req.query.code;
+					if (req.query.code){
+						param.code = req.query.code;
+					}
+
+					price.find(param)
+						.populate({path:'matches', match:{"terminal":req.params.terminal}})
+						.sort({terminal:1,code:1})
+						.exec(function (err, prices) {
+							if(!err) {
+								res.send(200, {status:'OK', data:prices});
+							} else {
+								console.error('%s - Error: %s', dateTime.getDatetime(), err);
+								res.send(500, {status:'ERROR', data: err});
+							}
+						});
 				}
-
-				price.find(param)
-					.populate({path:'matches', match:{"terminal":req.params.terminal}})
-					.sort({terminal:1,code:1})
-					.exec(function (err, prices) {
-						if(!err) {
-							res.send(200, {status:'OK', data:prices});
-						} else {
-							console.error('%s - Error: %s', dateTime.getDatetime(), err);
-							res.send(500, {status:'ERROR', data: err});
-						}
-					});
 
 			}
 		});
 	}
 
 	function getMatches (req, res){
-
 		'use strict';
+
 		var incomingToken = req.headers.token;
 		Account.verifyToken(incomingToken, function(err, usr) {
 			if (err){
 				console.error('%s - Error: %s', dateTime.getDatetime(), err);
 				res.send(500, {status:"ERROR", data:"Invalid or missing Token"});
 			} else {
+				var paramTerminal = req.params.terminal;
 
-				var param = [
-					{
-						$match: {terminal:	req.params.terminal }
-					},
-					{	$unwind: '$match' }
-				];
+				if (usr.terminal !== 'AGP' && usr.terminal !== paramTerminal){
+					var errMsg = util.format('%s - Error: %s', dateTime.getDatetime(), 'La terminal recibida por parámetro es inválida para el token.');
+					console.error(errMsg);
+					res.send(500, {status:"ERROR", data: errMsg});
+				} else {
+					var param = [
+						{
+							$match: {terminal:	paramTerminal }
+						},
+						{	$unwind: '$match' }
+					];
 
-				var s = MatchPrice.aggregate(param);
-				s.exec(function (err, matches) {
-					if(!err) {
+					var s = MatchPrice.aggregate(param);
+					s.exec(function (err, matches) {
+						if(!err) {
 
-						price.find({$or: [{terminal:"AGP"}, {terminal: req.params.terminal }]})
-							.exec(function (err, prices) {
-								if(!err) {
-									var result = {};
-									var Enumerable = require('linq');
-									var response = Enumerable.from(matches)
-										.join(Enumerable.from(prices), '$.price.id', '$._id.id', function (match, price){
-											match.description = price.description;
-											return match;
-										}).toArray();
-									response.forEach(function (item){
-										result[item.match] = item.description;
-									});
+							price.find({$or: [{terminal:"AGP"}, {terminal: paramTerminal }]})
+								.exec(function (err, prices) {
+									if(!err) {
+										var result = {};
+										var Enumerable = require('linq');
+										var response = Enumerable.from(matches)
+											.join(Enumerable.from(prices), '$.price.id', '$._id.id', function (match, price){
+												match.description = price.description;
+												return match;
+											}).toArray();
+										response.forEach(function (item){
+											result[item.match] = item.description;
+										});
 
-									res.send(200, {status:'OK', data: result});
+										res.send(200, {status:'OK', data: result});
 
-								} else {
-									console.error('%s - Error: %s', dateTime.getDatetime(), err);
-									res.send(500, {status:'ERROR', data: err});
-								}
-							});
+									} else {
+										console.error('%s - Error: %s', dateTime.getDatetime(), err);
+										res.send(500, {status:'ERROR', data: err});
+									}
+								});
 
-
-					} else {
-						console.error('%s - Error: %s', dateTime.getDatetime(), err);
-						res.send(500, {status:'ERROR', data: err});
-					}
-				});
-
-
+						} else {
+							console.error('%s - Error: %s', dateTime.getDatetime(), err);
+							res.send(500, {status:'ERROR', data: err});
+						}
+					});
+				}
 			}
 		});
 	}
