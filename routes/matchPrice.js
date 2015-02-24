7ae3e98caaeb21e0a30189e2f8aa2f654d7fa632
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-02-24 12:05:51 -0300

    Se agrega ruta "/matchprices/price/:terminal" devuelve solo los matcheprices codes con el precio de prices

--- routes/matchPrice.js
+++ routes/matchPrice.js
@@ -8,7 +8,7 @@ module.exports = function (app, log){
 	var MatchPrice = require('../models/matchPrice.js');
 	var Invoice = require('../models/invoice.js');
 	var util = require('util');
-	var price = require('../models/price.js');
+	var Price = require('../models/price.js');
 	var moment = require('moment');
 
 	var path = require('path');
@@ -46,12 +46,73 @@ module.exports = function (app, log){
 							param.rate = {$exists:true};
 					}
 
-					price.find(param, {topPrices : {$slice:-1}})
+					Price.find(param, {topPrices : {$slice:-1}})
 						.populate({path:'matches', match:{"terminal":req.params.terminal}})
 						.sort({terminal:1,code:1})
 						.exec(function (err, prices) {
 							if(!err) {
 								res.send(200, {status:'OK', data:prices});
+							} else {
+								log.logger.error('Error: %s', err.message);
+								res.send(500, {status:'ERROR', data: err.message});
+							}
+						});
+				}
+
+			}
+		});
+	}
+
+	function getMatchPricesPrice (req, res){
+
+		var incomingToken = req.headers.token;
+		Account.verifyToken(incomingToken, function(err, usr) {
+			if (err){
+				log.logger.error('Error: %s', err);
+				res.send(500, {status:"ERROR", data:"Invalid or missing Token"});
+			} else {
+				var paramTerminal = req.params.terminal;
+
+				if (usr.terminal !== 'AGP' && usr.terminal !== paramTerminal) {
+					var errMsg = util.format('Error: %s', 'La terminal recibida por parámetro es inválida para el token.');
+					log.logger.error(errMsg);
+					res.send(500, {status:"ERROR", data: errMsg});
+				} else {
+					var ter = (usr.role === 'agp') ? paramTerminal : usr.terminal;
+					var param = {
+						$or : [
+							{terminal:	"AGP"},
+							{terminal:	ter}
+						]
+					};
+
+					if (req.query.code){
+						param.code = req.query.code;
+					}
+
+					if (req.query.onlyRates){
+						if (req.query.onlyRates !== false)
+							param.rate = {$exists:true};
+					}
+
+					Price.aggregate([
+							{$match: param},
+							{$unwind: '$matches'},
+							{$project: {topPrices:true, matches: true}}
+							])
+						.exec(function (err, prices) {
+							if(!err) {
+								MatchPrice.find({}, {code:true, price: true}, function (err, matches){
+									var Enumerable = require('linq');
+									var response = [];
+									Enumerable.from(matches)
+										.join(Enumerable.from(prices), '$.price.id', '$._id.id', function (match, price){
+											response.push({code: match.code,
+															topPrices : price.topPrices})
+										}
+									).toArray();
+									res.send(200, {status:'OK', data: response});
+								});
 							} else {
 								log.logger.error('Error: %s', err.message);
 								res.send(500, {status:'ERROR', data: err.message});
@@ -89,7 +150,7 @@ module.exports = function (app, log){
 					s.exec(function (err, matches) {
 						if(!err) {
 
-							price.find({$or: [{terminal:"AGP"}, {terminal: paramTerminal }]})
+							Price.find({$or: [{terminal:"AGP"}, {terminal: paramTerminal }]})
 								.exec(function (err, prices) {
 									if(!err) {
 										var result = {};
@@ -213,7 +274,7 @@ module.exports = function (app, log){
 
 		async.forEachSeries( matches, function(match, asyncCallback){
 
-			price.findOne({_id: match._idPrice}, function(err, priceItem){
+			Price.findOne({_id: match._idPrice}, function(err, priceItem){
 				if(!err && priceItem) {
 					if (match._id !== undefined && match._id != null){
 						MatchPrice.findOne({_id: match._id}, function(err, matchItem){
@@ -250,6 +311,7 @@ module.exports = function (app, log){
 	}
 
 	app.get('/matchprices/:terminal', getMatchPrices);
+	app.get('/matchprices/price/:terminal', getMatchPricesPrice);
 	app.get('/matches/:terminal', getMatches);
 	app.get('/noMatches/:terminal', getNoMatches);
 	app.post('/matchprice', addMatchPrice);
