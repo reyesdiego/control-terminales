4cf66bd90b108095d715585e8a89dcb373f2c409
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-03-02 16:20:56 -0300

    Se instala al ultimo Express 3.20 (4.0?) y se saca app.use(app.router) que fue deprecado. En testeo

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.configure(function () {
 
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(app.router);
+
+	//app.use(app.router);
 
 //	app.use(passport.initialize());
 
