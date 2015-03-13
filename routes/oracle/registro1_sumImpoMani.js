7115b0483bb102217c7fc9930a2ab7453170a5b4
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-03-13 16:40:36 -0300

    Emprolijo codigo y agrego ruta en SumariaImpo /shipstrips

--- routes/oracle/registro1_sumImpoMani.js
+++ routes/oracle/registro1_sumImpoMani.js
@@ -15,102 +15,101 @@ module.exports = function (log, pool){
 		pool.acquire(function(err, connection) {
 			if (err) {
 				console.log(err, "Error acquiring from pool.");
-				return;
+				res.status(500).json({ status:'ERROR', data: err });
+			} else {
+				var oracleUtils = require('../../include/oracle.js')
+				oracleUtils = new oracleUtils();
+				var orderBy = oracleUtils.orderBy(req.query.order);
+
+				var strWhere = '';
+				var skip = parseInt(req.params.skip, 10);
+				var limit = parseInt(req.params.limit, 10);
+				var strSql = "SELECT * FROM " +
+					" (SELECT " +
+					"		ID, " +
+					"		TIPOREGISTRO, " +
+					"		SUMARIA, " +
+					"		SUM_ANIO, " +
+					"		SUM_ADUANA, " +
+					"		SUM_TIPO, " +
+					"		SUM_NRO, " +
+					"		SUM_LETRA_CTRL, " +
+					"		CUITATA, " +
+					"		NOMBREATA, " +
+					"		ESTADO, " +
+					"		FECHAREGISTRO, " +
+					"		FECHAARRIBO, " +
+					"		TRANSPORTEVACIO, " +
+					"		PAISPROCEDENCIA, " +
+					"		TRANSPORTISTA, " +
+					"		PAISTRANSPORTISTA, " +
+					"		COMENTARIO, " +
+					"		IMPO_EXPO, " +
+					"		DESCONSOLIDADO, " +
+					"		TITULO, " +
+					"		MERCADERIAABORDO, " +
+					"		VIA, " +
+					"		NACIONALIDADMEDIOTRANSPORTE, " +
+					"		LUGAROPERATIVO, " +
+					"		LUGARDEGIRO, " +
+					"		NOMBREBUQUE, " +
+					"		REGISTRADO_POR, " +
+					"		REGISTRADO_EN, " +
+					"		ROW_NUMBER() OVER (ORDER BY " + orderBy + ") R " +
+					"	FROM V_REGISTRO1_SUMIMPOMANI %s) " +
+					"WHERE R BETWEEN :1 and :2	";
+
+				if (req.query.buque)
+					strWhere += util.format(" NOMBREBUQUE = '%s' ", req.query.buque);
+
+				strSql = util.format(strSql, strWhere);
+				connection.execute(strSql,[skip+1, skip+limit], function (err, data){
+					if (err){
+						pool.destroy(connection);
+						res.status(500).json({ status:'ERROR', data: err.message });
+					} else {
+						strSql = "SELECT COUNT(*) AS TOTAL FROM REGISTRO1_SUMIMPOMANI ";
+						if (strWhere !== '')
+							strSql += util.format(" %s", strWhere);
+						connection.execute(strSql, [], function (err, dataCount){
+							pool.release(connection);
+							if (err){
+								res.status(500).json({ status:'ERROR', data: err.message });
+							} else {
+								var total = dataCount[0].TOTAL;
+								var result = {
+									status:'OK',
+									totalCount : total,
+									page: skip,
+									pageCount : (limit > total) ? total : limit,
+									data: data };
+								res.status(200).json(result);
+							}
+						});
+					}
+				});
 			}
-
-			var oracleUtils = require('../../include/oracle.js')
-			oracleUtils = new oracleUtils();
-			var orderBy = oracleUtils.orderBy(req.query.order);
-
-			var strWhere = '';
-			var skip = parseInt(req.params.skip, 10);
-			var limit = parseInt(req.params.limit, 10);
-			var strSql = "SELECT * FROM " +
-				" (SELECT " +
-				"		ID, " +
-				"		TIPOREGISTRO, " +
-				"		SUMARIA, " +
-				"		SUM_ANIO, " +
-				"		SUM_ADUANA, " +
-				"		SUM_TIPO, " +
-				"		SUM_NRO, " +
-				"		SUM_LETRA_CTRL, " +
-				"		CUITATA, " +
-				"		NOMBREATA, " +
-				"		ESTADO, " +
-				"		FECHAREGISTRO, " +
-				"		FECHAARRIBO, " +
-				"		TRANSPORTEVACIO, " +
-				"		PAISPROCEDENCIA, " +
-				"		TRANSPORTISTA, " +
-				"		PAISTRANSPORTISTA, " +
-				"		COMENTARIO, " +
-				"		IMPO_EXPO, " +
-				"		DESCONSOLIDADO, " +
-				"		TITULO, " +
-				"		MERCADERIAABORDO, " +
-				"		VIA, " +
-				"		NACIONALIDADMEDIOTRANSPORTE, " +
-				"		LUGAROPERATIVO, " +
-				"		LUGARDEGIRO, " +
-				"		NOMBREBUQUE, " +
-				"		REGISTRADO_POR, " +
-				"		REGISTRADO_EN, " +
-				"		ROW_NUMBER() OVER (ORDER BY " + orderBy + ") R " +
-				"	FROM V_REGISTRO1_SUMIMPOMANI %s) " +
-				"WHERE R BETWEEN :1 and :2	";
-
-			if (req.query.buque)
-				strWhere += util.format(" NOMBREBUQUE = '%s' ", req.query.buque);
-
-			strSql = util.format(strSql, strWhere);
-			connection.execute(strSql,[skip+1, skip+limit], function (err, data){
-				if (err){
-					pool.destroy(connection);
-					res.status(500).json({ status:'ERROR', data: err.message });
-				} else {
-					strSql = "SELECT COUNT(*) AS TOTAL FROM REGISTRO1_SUMIMPOMANI ";
-					if (strWhere !== '')
-						strSql += util.format(" %s", strWhere);
-					connection.execute(strSql, [], function (err, dataCount){
-						pool.release(connection);
-						if (err){
-							res.status(500).json({ status:'ERROR', data: err.message });
-						} else {
-							var total = dataCount[0].TOTAL;
-							var result = {
-								status:'OK',
-								totalCount : total,
-								page: skip,
-								pageCount : (limit > total) ? total : limit,
-								data: data };
-							res.status(200).json(result);
-						}
-					});
-				}
-			});
 
 		});
 	}
 
-	function getSumariaImpoContenedor (req, res) {
+	function getByContenedor (req, res) {
 		pool.acquire(function(err, connection) {
 			if (err) {
 				console.log(err, "Error acquiring from pool.");
-				return;
-			}
+				res.status(500).json({ status:'ERROR', data: err });
+			} else {
 
-			var strWhere = '';
-			var strSql = "	select sumaria, conocimiento " +
-						"	from registro4_sumimpomani " +
-						"	where contenedor = :1";
+				var strSql = "	select sumaria, conocimiento " +
+					"	from registro4_sumimpomani " +
+					"	where contenedor = :1";
 
-			connection.execute(strSql,[req.params.contenedor], function (err, dataSum){
-				if (err){
-					pool.destroy(connection);
-					res.send(500, { status:'ERROR', data: err.message });
-				} else {
-					strSql = 'SELECT r1.SUMARIA, ' +
+				connection.execute(strSql,[req.params.contenedor], function (err, dataSum){
+					if (err){
+						pool.destroy(connection);
+						res.send(500, { status:'ERROR', data: err.message });
+					} else {
+						strSql = 'SELECT r1.SUMARIA, ' +
 							'SUBSTR( r1.SUMARIA, 0, 2) as SUM_ANIO, ' +
 							'SUBSTR( r1.SUMARIA, 3, 3) as SUM_ADUANA, ' +
 							'SUBSTR( r1.SUMARIA, 6, 4) as SUM_TIPO, ' +
@@ -124,23 +123,51 @@ module.exports = function (log, pool){
 							'INNER JOIN PAISES p1 on p1.ID = PAISPROCEDENCIA ' +
 							'WHERE r2.CONOCIMIENTO = :1 ';
 
-					var result;
-					if (dataSum.length > 0) {
-						connection.execute(strSql,[dataSum[0].CONOCIMIENTO], function (err, data) {
-							pool.release(connection);
+						var result;
+						if (dataSum.length > 0) {
+							connection.execute(strSql,[dataSum[0].CONOCIMIENTO], function (err, data) {
+								pool.release(connection);
+								result = {
+									status:'OK',
+									data: data };
+								res.status(200).json(result);
+							});
+						} else {
 							result = {
 								status:'OK',
-								data: data };
+								data: [] };
 							res.status(200).json(result);
-						});
+						}
+					}
+				});
+			}
+
+		});
+	}
+
+	function getShipsTrips (req, res) {
+		pool.acquire(function(err, connection) {
+			if (err) {
+				console.log(err, "Error acquiring from pool.");
+				res.status(500).json({ status:'ERROR', data: err });
+			} else {
+				var strSql = "select nombrebuque, fechaarribo, count(*) cnt " +
+							"	from registro1_sumimpomani " +
+							"	group by nombrebuque, fechaarribo " +
+							"	order by nombrebuque,fechaarribo";
+
+				connection.execute(strSql, [],function (err, data){
+					if (err){
+						pool.destroy(connection);
+						res.send(500, { status:'ERROR', data: err });
 					} else {
-						result = {
-							status:'OK',
-							data: [] };
+						var Enumerate = require("linq");
+						var dataQ = Enumerate.from(data);
+						var result = dataQ.toArray();
 						res.status(200).json(result);
 					}
-				}
-			});
+				});
+			}
 		});
 	}
 
@@ -149,10 +176,8 @@ module.exports = function (log, pool){
 		next();
 	});
 	router.get('/registro1_sumimpomani/:skip/:limit', getRegistro1_sumimpomani);
-	router.get('/sumariaImpo/:contenedor', getSumariaImpoContenedor);
-
-//	app.get('/afip/registro1_sumimpomani/:skip/:limit', getRegistro1_sumimpomani)
-//	app.get('/afip/sumariaImpo/:contenedor', getSumariaImpoContenedor)
+	router.get('/sumariaImpo/:contenedor', getByContenedor);
+	router.get('/registro1_sumimpomani/shipstrips', getShipsTrips);
 
 	return router;
 };
