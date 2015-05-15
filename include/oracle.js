94b8139a255bcc2fac0be0f82d964263bf5329f0
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-05-15 11:36:22 -0300

    JSLint - Tyding code

--- include/oracle.js
+++ include/oracle.js
@@ -3,30 +3,30 @@
  */
 
 
-var oracleUtils = function (){
-
-}
+var oracleUtils = function () {
+    'use strict';
+};
 
 oracleUtils.prototype = {
 
-	orderBy : function (order) {
-		var orderBy = '';
-		var orderType='ASC';
-		if (order) {
+    orderBy : function (order) {
+        'use strict';
+        var orderBy = '',
+            orderType = 'ASC';
+        if (order) {
 
-			var _orderBy = JSON.parse(order);
+            order = JSON.parse(order);
 
-			for (var i= 0, len = _orderBy.length; i < len; i++)
-				for (var prop in _orderBy[i]){
-					if (_orderBy[i][prop] === -1) orderType = 'DESC';
-					orderBy = prop + ' ' + orderType;
-				}
-		} else {
-			orderBy = 'ID ASC';
-		}
-		return orderBy;
-
-	}
+            for (var i= 0, len = order.length; i < len; i++)
+                for (var prop in order[i]){
+                    if (order[i][prop] === -1) orderType = 'DESC';
+                    orderBy = prop + ' ' + orderType;
+                }
+        } else {
+            orderBy = 'ID ASC';
+        }
+        return orderBy;
+    }
 }
 
 module.exports = oracleUtils;
