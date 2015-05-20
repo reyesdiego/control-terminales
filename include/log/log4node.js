0de2454fe4b17ff983b5e424ae614f15fd2583f7
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-05-20 10:40:36 -0300

    JSLint tyding code

--- include/log/log4node.js
+++ include/log/log4node.js
@@ -3,106 +3,112 @@
  */
 
 var log4njs = function (options) {
-	'use strict';
+    'use strict';
 
-	var self = this;
+    var self = this,
+        moment  = require('moment'),
+        winston = require('winston'),
+        fs      = require('fs'),
+        elapsed = 0,
+    // Logging levels
+        config = {
+            levels: {
+                silly: 0,
+                verbose: 1,
+                info: 2,
+                data: 3,
+                warn: 4,
+                debug: 5,
+                error: 6,
+                insert: 7,
+                update: 8,
+                delete: 9
+            },
+            colors: {
+                silly: 'magenta',
+                verbose: 'cyan',
+                info: 'green',
+                data: 'grey',
+                warn: 'yellow',
+                debug: 'blue',
+                error: 'red',
+                insert: 'yellow',
+                update: 'green',
+                delete: 'red'
+            }
+        };
 
-	var moment	=	require('moment');
-	var winston	=	require('winston');
-	var fs		=	require('fs');
+    var transports = [];
+    if (options.toConsole) {
+        transports.push(new (winston.transports.Console)({
+            colorize: true,
+            raw: false,
+            timestamp: true
+        }));
+    }
 
-	var elapsed = 0;
+    if (options.toFile) {
+        transports.push(
+            new (winston.transports.File)({ filename: options.path + options.filename })
+        );
+    }
 
-// Logging levels
-	var config = {
-		levels: {
-			silly: 0,
-			verbose: 1,
-			info: 2,
-			data: 3,
-			warn: 4,
-			debug: 5,
-			error: 6,
-			insert:7,
-			update:8,
-			delete:9
-		},
-		colors: {
-			silly: 'magenta',
-			verbose: 'cyan',
-			info: 'green',
-			data: 'grey',
-			warn: 'yellow',
-			debug: 'blue',
-			error: 'red',
-			insert: 'yellow',
-			update: 'green',
-			delete: 'red'
-		}
-	};
+    var logger = module.exports = new (winston.Logger)({
+        transports: transports,
+        levels: config.levels,
+        colors: config.colors
+    });
 
-	var transports = [];
-	if (options.toConsole)
-		transports.push(new (winston.transports.Console)({
-			colorize: true,
-			raw: false,
-			timestamp: true
-		}));
-	if (options.toFile)
-		transports.push(
-			new (winston.transports.File)({ filename: options.path + options.filename })
-		);
+    self.moment = moment;
+    self.logger = logger;
+    self.path = options.path;
+    self.filename = options.filename;
+    self.toConsole = options.toConsole;
+    self.toFile = options.toFile;
 
-	var logger = module.exports = new (winston.Logger)({
-		transports: transports,
-		levels: config.levels,
-		colors: config.colors
-	});
+    self.getFiles = function (callback) {
+        if (callback !== undefined && typeof(callback) === 'function') {
+            var logFiles = [];
+            fs.readdir(self.path, function (err, files) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                var pathFilename = '',
+                    stats;
+                files.forEach(function (item) {
+                    pathFilename = self.path + '/' + item;
+                    stats = fs.statSync(pathFilename);
+                    logFiles.push({title: item, mtime: stats.mtime, url: pathFilename});
+                });
+                logFiles.sort(function compare(a, b) {
+                    var time1 = moment(a.mtime),
+                        time2 = moment(b.mtime),
+                        result = 0;
 
-	self.moment = moment;
-	self.logger = logger;
-	self.path = options.path;
-	self.filename = options.filename;
-	self.toConsole = options.toConsole;
-	self.toFile = options.toFile;
+                    if (time1 > time2) {
+                        result = -1;
+                    }
+                    if (time1 < time2) {
+                        result = 1;
+                    }
 
-	self.getFiles = function (callback){
-		if (callback !== undefined && typeof (callback) === 'function'){
-			var logFiles = [];
-			fs.readdir(self.path, function (err, files) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				var pathFilename = '';
-				var stats;
-				files.forEach(function (item){
-					pathFilename = self.path + '/' +item;
-					stats = fs.statSync(pathFilename);
-					logFiles.push({title: item, mtime: stats.mtime, url: pathFilename});
-				});
-				logFiles.sort(function compare(a,b) {
-					var time1 = moment(a.mtime);
-					var time2 = moment(b.mtime);
-					if (time1 > time2)
-						return -1;
-					if (time1 < time2)
-						return 1;
-					return 0;
-				});
-				return callback(logFiles);
-			});
-		}
-	}
+                    return result;
+                });
 
-	self.startElapsed = function (){
-		elapsed = moment();
-	}
-	self.getElapsed = function (){
-		var elapsedEnd = moment();
-		elapsed = elapsed.diff(elapsedEnd) * (-1);
-		return elapsed;
-	}
+                return callback(logFiles);
+            });
+        }
+    };
+
+    self.startElapsed = function () {
+        elapsed = moment();
+    };
+    self.getElapsed = function () {
+        var elapsedEnd = moment();
+        elapsed = elapsed.diff(elapsedEnd) * (-1);
+        return elapsed;
+    };
 
 };
 
