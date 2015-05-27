d0aadff048e11add398e6115339eb58609650ed9
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-05-27 11:19:10 -0300

    JSLint Tyding

--- routes/unitType.js
+++ routes/unitType.js
@@ -2,41 +2,43 @@
  * Created by diego on 11/17/14.
  */
 
-module.exports = function (log){
+module.exports = function (log) {
+    'use strict';
 
-	var express = require('express');
-	var router = express.Router();
+    var express = require('express'),
+        router = express.Router(),
+        response;
 
-	function getUnitTypes(req, res){
-		var Unit = require('../models/unitType.js');
+    function getUnitTypes(req, res) {
+        var Unit = require('../models/unitType.js');
 
-		Unit.find({}, function (err, data){
-			if (err){
-				res.status(500).send({status:"ERROR", data: err.message});
-			} else {
-				var result = data;
-				if (req.query.type === 'array'){
-					result={};
-					data.forEach(function (item){
-						result[item._id] = item.description;
-					});
-				}
-				var response = {
-					status:		'OK',
-					totalCount:	data.length,
-					data: result
-				};
-				res.status(200).send(response);
-			}
-		});
-	}
+        Unit.find({}, function (err, data) {
+            if (err) {
+                res.status(500).send({status: "ERROR", data: err.message});
+            } else {
+                var result = data;
+                if (req.query.type === 'array') {
+                    result = {};
+                    data.forEach(function (item) {
+                        result[item._id] = item.description;
+                    });
+                }
+                response = {
+                    status: 'OK',
+                    totalCount: data.length,
+                    data: result
+                };
+                res.status(200).send(response);
+            }
+        });
+    }
 /*
-	router.use(function timeLog(req, res, next){
-		log.logger.info('Time: %s', Date.now());
-		next();
-	});
+router.use(function timeLog(req, res, next){
+    log.logger.info('Time: %s', Date.now());
+    next();
+});
 */
-	router.get('/', getUnitTypes);
+    router.get('/', getUnitTypes);
 
-	return router;
+    return router;
 }
