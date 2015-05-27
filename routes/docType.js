879802c1cc7fbf9b9cb27aa12302edc9a3fd1682
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-05-27 11:11:54 -0300

    JSLint Tyding

--- routes/docType.js
+++ routes/docType.js
@@ -2,42 +2,44 @@
  * Created by diego on 10/30/14.
  */
 
-module.exports = function (log){
+module.exports = function (log) {
+    'use strict';
 
-	var express = require('express');
-	var router = express.Router();
+    var express = require('express'),
+        router = express.Router();
 
-	function getDocTypes(req, res){
-		var Doc = require('../models/docType.js');
+    function getDocTypes(req, res) {
+        var Doc = require('../models/docType.js'),
+            response;
 
-		Doc.find({}, function (err, data){
-			if (err){
-				res.status(500).send({status:"ERROR", data: err.message});
-			} else {
-				var result = data;
-				if (req.query.type === 'array'){
-					result={};
-					data.forEach(function (item){
-						result[item._id] = item.description;
-					});
-				}
-				var response = {
-					status:		'OK',
-					totalCount:	data.length,
-					data: result
-				};
-				res.status(200).send(response);
-			}
-		});
-	}
+        Doc.find({}, function (err, data) {
+            if (err) {
+                res.status(500).send({status: "ERROR", data: err.message});
+            } else {
+                var result = data;
+                if (req.query.type === 'array') {
+                    result = {};
+                    data.forEach(function (item) {
+                        result[item._id] = item.description;
+                    });
+                }
+                response = {
+                    status: 'OK',
+                    totalCount: data.length,
+                    data: result
+                };
+                res.status(200).send(response);
+            }
+        });
+    }
+
 /*
-	router.use(function timeLog(req, res, next){
-		log.logger.info('Time: %s', Date.now());
-		next();
-	});
+router.use(function timeLog(req, res, next){
+    log.logger.info('Time: %s', Date.now());
+    next();
+});
 */
-	router.get('/', getDocTypes);
+    router.get('/', getDocTypes);
 
-	return router;
-
+    return router;
 }
