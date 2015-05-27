65edf2fb034aee3ead0ab8e5fa4b99b88f563616
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-05-27 11:17:39 -0300

    JSLint Tyding

--- routes/state.js
+++ routes/state.js
@@ -2,45 +2,47 @@
  * Created by diego on 12/10/14.
  */
 
-module.exports = function (log){
+module.exports = function (log) {
+    'use strict';
 
-	var express = require('express');
-	var router = express.Router();
+    var express = require('express'),
+        router = express.Router();
 
-	function getStates(req, res, next){
-		var State = require('../models/state.js');
+    function getStates(req, res, next) {
+        var State = require('../models/state.js'),
+            response;
 
-		State.find({}, function (err, data){
-			if (err){
-				res.status(500).send({status:"ERROR", data: err.message});
-			} else {
-				var result = data;
-				if (req.query.type === 'array'){
-					result={};
-					data.forEach(function (item){
-						result[item._id] = {
-							name: item.name,
-							description: item.description,
-							type: item.type
-						};
-					});
-				}
-				var response = {
-					status:		'OK',
-					totalCount:	data.length,
-					data: result
-				};
-				res.status(200).json(response);
-			}
-		});
-	}
+        State.find({}, function (err, data) {
+            if (err) {
+                res.status(500).send({status: "ERROR", data: err.message});
+            } else {
+                var result = data;
+                if (req.query.type === 'array') {
+                    result = {};
+                    data.forEach(function (item) {
+                        result[item._id] = {
+                            name: item.name,
+                            description: item.description,
+                            type: item.type
+                        };
+                    });
+                }
+                response = {
+                    status: 'OK',
+                    totalCount: data.length,
+                    data: result
+                };
+                res.status(200).json(response);
+            }
+        });
+    }
 /*
-	router.all(function (req, res, next){
-		log.logger.info('Time: %s', Date.now());
-		next();
-	});
+router.all(function (req, res, next){
+    log.logger.info('Time: %s', Date.now());
+    next();
+});
 */
-	router.get('/', getStates);
-	return router;
+    router.get('/', getStates);
+    return router;
 
 }
