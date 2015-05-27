f94605d3cd9ea309ccf69cbf662bee5893b08a4d
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-05-27 11:20:41 -0300

    JSLint Tyding

--- routes/voucherType.js
+++ routes/voucherType.js
@@ -2,41 +2,43 @@
  * Created by diego on 6/3/14.
  */
 
-module.exports = function (log){
+module.exports = function (log) {
+    'use strict';
 
-	var express = require('express');
-	var router = express.Router();
+    var express = require('express'),
+        router = express.Router();
 
-	function getVoucherTypes(req, res){
-		var Voucher = require('../models/voucherType.js');
+    function getVoucherTypes(req, res) {
+        var Voucher = require('../models/voucherType.js'),
+            response;
 
-		Voucher.find({}, function (err, data){
-			if (err){
-				res.status(500).send({status:"ERROR", data: err.message});
-			} else {
-				var result = data;
-				if (req.query.type === 'array'){
-					result={};
-					data.forEach(function (item){
-						result[item._id] = item.description;
-					});
-				}
-				var response = {
-					status:		'OK',
-					totalCount:	data.length,
-					data: result
-				};
-				res.status(200).send(response);
-			}
-		});
-	}
+        Voucher.find({}, function (err, data) {
+            if (err) {
+                res.status(500).send({status: "ERROR", data: err.message});
+            } else {
+                var result = data;
+                if (req.query.type === 'array') {
+                    result = {};
+                    data.forEach(function (item) {
+                        result[item._id] = item.description;
+                    });
+                }
+                response = {
+                    status: 'OK',
+                    totalCount: data.length,
+                    data: result
+                };
+                res.status(200).send(response);
+            }
+        });
+    }
 /*
-	router.use(function timeLog(req, res, next){
-		log.logger.info('Time: %s', Date.now());
-		next();
-	});
+router.use(function timeLog(req, res, next){
+    log.logger.info('Time: %s', Date.now());
+    next();
+});
 */
-	router.get('/', getVoucherTypes);
+    router.get('/', getVoucherTypes);
 
-	return router;
+    return router;
 }
