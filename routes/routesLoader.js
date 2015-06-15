e9cf344889e0195725f30c0892ad668f06be38cb
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-15 10:25:57 -0300

    JSLint tyding code

--- routes/routesLoader.js
+++ routes/routesLoader.js
@@ -4,6 +4,19 @@
 
 module.exports = function (app, log, io, mongoose, pool) {
     'use strict';
+    var serverMain,
+        state,
+        match,
+        price,
+        comment,
+        appointment,
+        appointmentEmailQueue,
+        docType,
+        unitType,
+        task,
+        voucherType,
+        gate,
+        invoice
 
     function isValidToken(req, res, next) {
 
@@ -21,43 +34,43 @@ module.exports = function (app, log, io, mongoose, pool) {
         });
     }
 
-    var serverMain = require('./server')(log, app, mongoose);
+    serverMain = require('./server')(log, app, mongoose);
     app.use('/', serverMain);
 
-    var state = require('./state')(log);
+    state = require('./state')(log);
     app.use('/states', state);
 
-    var match = require('./matchPrice')(log);
+    match = require('./matchPrice')(log);
     app.use('/matchPrices', isValidToken, match);
 
-    var price = require('./price')(log);
+    price = require('./price')(log);
     app.use('/prices', isValidToken, price);
 
-    var comment = require('./comment')(log);
+    comment = require('./comment')(log);
     app.use('/comments', isValidToken, comment);
 
-    var appointment = require('./appointment')(log, io, app);
+    appointment = require('./appointment')(log, io, app);
     app.use('/appointments', isValidToken, appointment);
 
-    var appointmentEmailQueue = require('./appointmentEmailQueue')(log, io);
+    appointmentEmailQueue = require('./appointmentEmailQueue')(log, io);
     app.use('/appointmentEmailQueues', isValidToken, appointmentEmailQueue);
 
-    var docType = require('./docType')(log);
+    docType = require('./docType')(log);
     app.use('/docTypes', docType);
 
-    var unitType = require('./unitType')(log);
+    unitType = require('./unitType')(log);
     app.use('/unitTypes', unitType);
 
-    var task = require('./task')(log);
+    task = require('./task')(log);
     app.use('/tasks', isValidToken, task);
 
-    var voucherType = require('./voucherType')(log);
+    voucherType = require('./voucherType')(log);
     app.use('/voucherTypes', voucherType);
 
-    var gate = require('./gate')(log, io, app);
+    gate = require('./gate')(log, io, app);
     app.use('/gates', isValidToken, gate);
 
-    var invoice = require('./invoice')(log, io, pool, app);
+    invoice = require('./invoice')(log, io, pool, app);
     app.use('/invoices', isValidToken, invoice);
 
 }
