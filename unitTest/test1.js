f62916de9f56094b31bf372505c59c59ff967e93
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-19 16:36:41 -0300

    Agrego más Unit Test

--- unitTest/test1.js
+++ unitTest/test1.js
@@ -1,21 +1,23 @@
 /**
- * Created by diego on 11/06/15.
- */
+* Created by diego on 11/06/15.
+*/
 
 var should = require('should');
 
-describe('Array', function(){
-	describe('#indexOf()', function(){
-		it('should return -1 when the value is not present', function(done){
-			[1,2,3,9].indexOf(5).should.equal(-1);
-			[1,2,3].indexOf(0).should.equal(-1);
-			done();
-		});
-		it('should return "B" when the word start with B', function(done){
-			"ueno".substr(0,1).should.equal("B");
-			"Baueno".substr(0,1).should.equal("B");
-			done()
-		})
+describe('Array', function () {
+    'use strict';
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function (done) {
+            [1, 2, 3, 9].indexOf(5).should.equal(-1);
+            [1, 2, 3].indexOf(0).should.equal(-1);
+            done();
+        });
+        it('should return "B" when the word start with B', function (done) {
+            "Bueno".substr(0, 1).should.equal("B");
+            "Baueno".substr(0, 1).should.equal("B");
+            done();
+        });
 
-	});
-})
+    });
+
+});
--- unitTest/testRest.js
+++ unitTest/testRest.js
@@ -5,56 +5,55 @@
 var should = require('should'),
     assert = require('assert'),
     request = require('supertest'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    config = require('../config/config.js');
 
-describe('Agp Api', function () {
+describe('AgpApi Rest', function () {
     'use strict';
     var url = 'http://localhost:8080';
     // within before() you can run all the operations that are needed to setup your tests. In this case
     // I want to create a connection with the database, and when I'm done, I call done().
     before(function (done) {
         // In our tests we use the test db
-    //mongoose.connect(config.db.mongodb);
+        //mongoose.connect(config.mongo_url, config.mongo_optsl);
+        //mongoose.connection.on('connected', function () {
+        //    done();
+        //});
         done();
     });
-    // use describe to give a title to your test suite, in this case the tile is "Account"
-    // and then specify a function in which we are going to declare all the tests
-    // we want to run. Each test starts with the function it() and as a first argument
-    // we have to provide a meaningful title for it, whereas as the second argument we
-    // specify a function that takes a single parameter, "done", that we will use
-    // to specify when our test is completed, and that's what makes easy
-    // to perform async test!
-    describe('Routes', function () {
-        it('should return error if / route not exists', function (done) {
 
-            request(url)
-                .get('/')
-                .expect(200)
-                .expect('Content-Type', /text\/html/)
-                // end handles the response
-                .end(function (err, res) {
-                    if (err) {
-                        throw err;
-                    }
+    it('/ get | should return error if route not exists', function (done) {
+
+        request(url)
+            .get('/')
+            .expect(200)
+            .expect('Content-Type', /text\/html/)
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
 //                    this is should.js syntax, very clear
 //                    res.should.have.status(200);
-                    done();
-                });
-        });
+                done();
+            });
+    });
 
-        it('should return error if /accounts route not exist and has not terminal and user', function (done) {
+    describe('Accounts', function () {
+
+        it('/agp/accounts - GET | should return status 403 if does not have token', function (done) {
 
             request(url)
-                .get('/accounts')
+                .get('/agp/accounts')
                 .expect('Content-Type', /json/)
-                .expect(200) //Status code
+                .expect(403) //Status code
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
                     // Should.js fluent syntax applied
-                    res.body.should.have.property('terminal');
-                    res.body.should.have.property('user');
+                    res.body.should.have.property('status');
+                    res.body.status.should.equal("ERROR");
+                    //res.body.length.should.not.equal(0);
 //                    res.body.a.should.equal(1);
 //                    res.body.b.should.equal(1);
 //                    res.body.creationDate.should.not.equal(null);
@@ -62,5 +61,348 @@ describe('Agp Api', function () {
                 });
         });
 
+        it('/login - POST | should return status 200 with user "agp" pass "agp431_" ', function (done) {
+
+            request(url)
+                .post('/login')
+//                .field('email', 'agp')
+                .send({email: "agp", password: "agp431_"})
+                .expect('Content-Type', /json/)
+                .expect(200) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // Should.js fluent syntax applied
+                    res.body.should.have.property('status');
+                    res.body.status.should.equal("OK");
+                    //res.body.length.should.not.equal(0);
+//                    res.body.a.should.equal(1);
+//                    res.body.b.should.equal(1);
+//                    res.body.creationDate.should.not.equal(null);
+                    done();
+                });
+        });
+
+    });
+
+    describe('Gates', function () {
+        describe('/gates/:terminal/:skip/:limit', function () {
+            it('GET | should return status 200', function (done) {
+
+                request(url)
+                    .get('/gates/BACTSSA/0/10')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 with token different as to :terminal', function (done) {
+
+                request(url)
+                    .get('/gates/TRP/0/10')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+
+            });
+            it('GET | should return status 403 with token different as to :terminal', function (done) {
+
+                request(url)
+                    .get('/gates/TRP/0/10')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+
+            });
+        });
+        describe('/gates/ByHour', function () {
+            it('GET | should return status 200', function (done) {
+
+                request(url)
+                    .get('/gates/ByHour')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 if does not have token', function (done) {
+
+                request(url)
+                    .get('/gates/ByHour')
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+            });
+        });
+        describe('/gates/ByMonth', function () {
+            it('GET | should return status 200', function (done) {
+                request(url)
+                    .get('/gates/ByMonth')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 if does not have token', function (done) {
+
+                request(url)
+                    .get('/gates/ByMonth')
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+            });
+        });
+        describe('/gates/:terminal/missingGates', function () {
+            it('GET | should return status 200', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/BACTSSA/missingGates')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 if does not have token', function (done) {
+
+                request(url)
+                    .get('/gates/BACTSSA/missingGates')
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 with token different as to :terminal', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/TRP/missingGates')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+
+            });
+        });
+        describe('/gates/:terminal/missingInvoices', function () {
+            it('GET | should return status 200', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/BACTSSA/missingInvoices')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 if does not have token', function (done) {
+
+                request(url)
+                    .get('/gates/BACTSSA/missingInvoices')
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 with token different as to :terminal', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/TRP/missingInvoices')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+
+            });
+        });
+        describe('/gates/:terminal/ships', function () {
+            it('GET | should return status 200', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/BACTSSA/ships')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 if does not have token', function (done) {
+
+                request(url)
+                    .get('/gates/BACTSSA/ships')
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 with token different as to :terminal', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/TRP/ships')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+
+            });
+        });
+        describe('/gates/:terminal/containers', function () {
+            it('GET | should return status 200', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/BACTSSA/containers')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('OK');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 if does not have token', function (done) {
+
+                request(url)
+                    .get('/gates/BACTSSA/containers')
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+            });
+            it('GET | should return status 403 with token different as to :terminal', function (done) {
+                this.timeout(30000);
+                request(url)
+                    .get('/gates/TRP/containers')
+                    .set('token', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvcG9ydGVfY3RzQGJhY3Rzc2EuY29tLmFyIn0.bBSqdwesWimlT8rFGA7sKLKbPdxauY8HdrTTt7BLpBU")
+                    .expect(403)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('status');
+                        res.body.status.should.equal('ERROR');
+                        done();
+                    });
+
+            });
+        });
     });
 });
