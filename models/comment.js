f2b0b67ecc7c392ae021bdde04c4ed9912e5f096
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-22 10:01:07 -0300

    JSLint Ttding code

--- models/comment.js
+++ models/comment.js
@@ -7,12 +7,12 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var commentSchema = new schema({
-	title	: {type: String, required: true},
-	comment	: {type: String},
-	user	: {type: String, required: true},
-	group	: {type: String, required: true},
-	state	: {type: String, required: true, enum: ['R', 'Y', 'G', 'C', 'T', 'E']},
-	invoice	: {type: mongoose.Schema.ObjectId, ref:'invoices'}
+    title: {type: String, required: true},
+    comment: {type: String},
+    user: {type: String, required: true},
+    group: {type: String, required: true},
+    state: {type: String, required: true, enum: ['R', 'Y', 'G', 'C', 'T', 'E']},
+    invoice: {type: mongoose.Schema.ObjectId, ref: 'invoices'}
 });
 
 module.exports = mongoose.model('comment', commentSchema);
