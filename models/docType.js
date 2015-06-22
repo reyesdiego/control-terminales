9547b2097e06a45a2ab286445844a44bf96a1248
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-22 11:05:56 -0300

    JSLint Tyding code

--- models/docType.js
+++ models/docType.js
@@ -5,8 +5,8 @@
 var mongoose = require("mongoose");
 
 var doc = new mongoose.Schema({
-	_id:		{type: Number},
-	description:{type: String, required:true}
+    _id: {type: Number},
+    description: {type: String, required: true}
 });
 
 module.exports = mongoose.model('doctypes', doc);
