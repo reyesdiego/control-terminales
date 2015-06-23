570c1fa4072d0e696c7a769a75b92efa650b3221
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-23 10:09:30 -0300

    JSLint tyding code

--- models/role.js
+++ models/role.js
@@ -5,24 +5,24 @@
 var mongoose = require('mongoose');
 
 var role = new mongoose.Schema({
-	name:	{type: String},
-	level:	{type: Number},
-	tasks:  [String]
+    name: {type: String},
+    level: {type: Number},
+    tasks: [String]
 });
 
 
 //role.post('init', function (doc) {
-//	console.log('%s has been initialized from the db', doc._id);
+// console.log('%s has been initialized from the db', doc._id);
 //});
 
-role.pre('save', function (next, done){
-	if (this.level){
-		if (this.level === 2) {
-			return next(new Error('Level can`t be 2'));
-		}
-	}
-	console.log('save pre ', this);
-	next();
+role.pre('save', function (next, done) {
+    if (this.level) {
+        if (this.level === 2) {
+            return next(new Error('Level can`t be 2'));
+        }
+    }
+    console.log('save pre ', this);
+    next();
 });
 
 module.exports = mongoose.model('roles', role);
