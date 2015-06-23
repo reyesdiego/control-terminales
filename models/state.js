4642675138955c7f06202bd1696a6a2e01030661
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-23 10:12:49 -0300

    JSLint tyding code

--- models/state.js
+++ models/state.js
@@ -5,10 +5,10 @@
 var mongoose = require("mongoose");
 
 var states = new mongoose.Schema({
-	_id:		{type: String},
-	name:		{type: String, required: true},
-	description:{type: String, required: true},
-	type:		{type: String, required: true, enum:['ERROR', 'OK', 'WARN', 'UNKNOWN']}
+    _id: {type: String},
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    type: {type: String, required: true, enum:['ERROR', 'OK', 'WARN', 'UNKNOWN']}
 });
 
 module.exports = mongoose.model('states', states);
