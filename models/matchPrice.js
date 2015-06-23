e4ecf01b8dc1f21ed58cc3ee0026c22c55e760e5
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-06-23 10:01:18 -0300

    JSLint tyding code

--- models/matchPrice.js
+++ models/matchPrice.js
@@ -4,10 +4,10 @@
 var mongoose = require('mongoose');
 
 var matchPrice = new mongoose.Schema({
-	terminal:	{type: String},
-	code:		{type: String},
-	match:		[{type: String}],
-	price:		{type: mongoose.Schema.ObjectId, ref:'prices'}
+    terminal: {type: String},
+    code: {type: String},
+    match: [{type: String}],
+    price: {type: mongoose.Schema.ObjectId, ref: 'prices'}
 });
 
 module.exports = mongoose.model('matchprices', matchPrice);
