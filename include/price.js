70d374dfd3f3dc1f6afea3b310728cfa0814bab2
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-07-23 16:38:45 -0300

    JSLint tyding code

--- include/price.js
+++ include/price.js
@@ -3,6 +3,7 @@
  */
 
 var price = function (terminal) {
+    'use strict';
 
     this.Price = require("../models/price.js");
     this.matchPrice = require("../models/matchPrice.js");
@@ -11,58 +12,60 @@ var price = function (terminal) {
 }
 
 price.prototype = {
-	rates: function (withDescription, callback){
+	rates: function (withDescription, callback) {
         var Enumerable = require('linq'),
+            selfMatchPrice,
+            params = [],
             self = this;
 
-		if (typeof withDescription === 'function'){
-			callback = withDescription;
-		}
+        if (typeof withDescription === 'function') {
+            callback = withDescription;
+        }
 
-		if (callback !== undefined){
-			var selfMatchPrice =this.matchPrice;
+        if (callback !== undefined) {
+            selfMatchPrice =this.matchPrice;
 
-			var params = [];
-			if (self.terminal !== undefined)
-				params.push({ $match: { terminal: self.terminal}});
-			else
-				params.push({ $match: { terminal:{$exists : 1}}});
+            if (self.terminal !== undefined) {
+                params.push({ $match: { terminal: self.terminal}});
+            } else {
+                params.push({ $match: { terminal:{$exists : 1}}});
+            }
 
-			params.push({ $project: {match:1, price:1}});
-			params.push({$unwind:'$match'});
-			selfMatchPrice.aggregate( params, function(err, data){
-				selfMatchPrice.populate(data, [{ path:'price', match:{rate:{$exists:1}} }], function (err, matchprices){
+            params.push({ $project: {match:1, price:1}});
+            params.push({$unwind:'$match'});
+            selfMatchPrice.aggregate( params, function(err, data){
+                selfMatchPrice.populate(data, [{ path:'price', match:{rate:{$exists:1}} }], function (err, matchprices) {
                     var ratesDesc = {},
                         result,
                         a;
 
-						if (err) {
-							if (typeof callback === 'function')
-								return callback(err);
-						} else {
-							result = Enumerable.from(matchprices)
-								.where(function (item){
-									return item.price != null;
-								});
+                        if (err) {
+                            if (typeof callback === 'function')
+                                return callback(err);
+                        } else {
+                            result = Enumerable.from(matchprices)
+                                .where(function (item){
+                                    return item.price != null;
+                                });
 
-							if (withDescription === true){
-								a = result.select(function(item){
-									ratesDesc[item.match] = item.price.description;
-									return item;
-								}).toArray();
-								result = ratesDesc;
-							} else {
-								result = result.select(function(item){
-									return item.match;
-								}).toArray();
-							}
-							if (typeof callback === 'function')
-								return callback( undefined, result);
-						}
-					});
-			});
-		}
-	}
+                            if (withDescription === true){
+                                a = result.select(function(item){
+                                    ratesDesc[item.match] = item.price.description;
+                                    return item;
+                                }).toArray();
+                                result = ratesDesc;
+                            } else {
+                                result = result.select(function(item){
+                                    return item.match;
+                                }).toArray();
+                            }
+                            if (typeof callback === 'function')
+                                return callback( undefined, result);
+                        }
+                    });
+            });
+        }
+    }
 }
 
 exports.price = price;
