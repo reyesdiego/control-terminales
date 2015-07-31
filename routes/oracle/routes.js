7f4e4344366564d0a9e5c62d62650f9b9ec93f85
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-07-31 08:54:54 -0300

    Agrego validacion de token para AFIP

--- routes/oracle/routes.js
+++ routes/oracle/routes.js
@@ -31,76 +31,76 @@ module.exports = function (log, app, pool) {
 
     });
     var registro1_afectacion = require('./registro1_afectacion')(log, pool);
-    app.use('/afip', registro1_afectacion);
+    app.use('/afip', isValidToken, registro1_afectacion);
 
     var registro1_detExpo = require('./registro1_detExpo')(log, pool);
-    app.use('/afip', registro1_detExpo);
+    app.use('/afip', isValidToken, registro1_detExpo);
 
     var registro1_detImpo = require('./registro1_detImpo')(log, pool);
-    app.use('/afip', registro1_detImpo);
+    app.use('/afip', isValidToken, registro1_detImpo);
 
     var registro1_solicitud = require('./registro1_solicitud')(log, pool);
-    app.use('/afip', registro1_solicitud);
+    app.use('/afip', isValidToken, registro1_solicitud);
 
     var registro1_sumExpoMane = require('./registro1_sumExpoMane')(log, pool);
-    app.use('/afip', registro1_sumExpoMane);
+    app.use('/afip', isValidToken, registro1_sumExpoMane);
 
     var registro1_sumImpoMani = require('./registro1_sumImpoMani')(log, pool);
-    app.use('/afip', registro1_sumImpoMani);
+    app.use('/afip', isValidToken, registro1_sumImpoMani);
 
     var registro1_remoTrb = require('./registro1_remoTrb')(log, pool);
-    app.use('/afip', registro1_remoTrb);
+    app.use('/afip', isValidToken, registro1_remoTrb);
 
     var registro2_afectacion = require('./registro2_afectacion')(log, pool);
-    app.use('/afip', registro2_afectacion);
+    app.use('/afip', isValidToken, registro2_afectacion);
 
     var registro2_detExpo = require('./registro2_detExpo')(log, pool);
-    app.use('/afip', registro2_detExpo);
+    app.use('/afip', isValidToken, registro2_detExpo);
 
     var registro2_detImpo = require('./registro2_detImpo')(log, pool);
-    app.use('/afip', registro2_detImpo);
+    app.use('/afip', isValidToken, registro2_detImpo);
 
     var registro2_solicitud = require('./registro2_solicitud')(log, pool);
-    app.use('/afip', registro2_solicitud);
+    app.use('/afip', isValidToken, registro2_solicitud);
 
     var registro2_sumExpoMane = require('./registro2_sumExpoMane')(log, pool);
-    app.use('/afip', registro2_sumExpoMane);
+    app.use('/afip', isValidToken, registro2_sumExpoMane);
 
     var registro2_sumImpoMane = require('./registro2_sumImpoMani')(log, pool);
-    app.use('/afip', registro2_sumImpoMane);
+    app.use('/afip', isValidToken, registro2_sumImpoMane);
 
     var registro2_remoTrb = require('./registro2_remoTrb')(log, pool);
-    app.use('/afip', registro2_remoTrb);
+    app.use('/afip', isValidToken, registro2_remoTrb);
 
     var registro3_afectacion = require('./registro3_afectacion')(log, pool);
-    app.use('/afip', registro3_afectacion);
+    app.use('/afip', isValidToken, registro3_afectacion);
 
     var registro3_detExpo = require('./registro3_detExpo')(log, pool);
-    app.use('/afip', registro3_detExpo);
+    app.use('/afip', isValidToken, registro3_detExpo);
 
     var registro3_detImpo = require('./registro3_detImpo')(log, pool);
-    app.use('/afip', registro3_detImpo);
+    app.use('/afip', isValidToken, registro3_detImpo);
 
     var registro3_solicitud = require('./registro3_solicitud')(log, pool);
-    app.use('/afip', registro3_solicitud);
+    app.use('/afip', isValidToken, registro3_solicitud);
 
     var registro3_sumExpoMane = require('./registro3_sumExpoMane')(log, pool);
-    app.use('/afip', registro3_sumExpoMane);
+    app.use('/afip', isValidToken, registro3_sumExpoMane);
 
     var registro3_sumImpoMane = require('./registro3_sumImpoMani')(log, pool);
-    app.use('/afip', registro3_sumImpoMane);
+    app.use('/afip', isValidToken, registro3_sumImpoMane);
 
     var registro3_remoTrb = require('./registro3_remoTrb')(log, pool);
-    app.use('/afip', registro3_remoTrb);
+    app.use('/afip', isValidToken, registro3_remoTrb);
 
     var registro4_sumExpoMane = require('./registro4_sumExpoMane')(log, pool);
-    app.use('/afip', registro4_sumExpoMane);
+    app.use('/afip', isValidToken, registro4_sumExpoMane);
 
     var registro4_sumImpoMane = require('./registro4_sumImpoMani')(log, pool);
-    app.use('/afip', registro4_sumImpoMane);
+    app.use('/afip', isValidToken, registro4_sumImpoMane);
 
     var registro5_sumExpoMane = require('./registro5_sumExpoMane')(log, pool);
-    app.use('/afip', registro5_sumExpoMane);
+    app.use('/afip', isValidToken, registro5_sumExpoMane);
 
 }
 
