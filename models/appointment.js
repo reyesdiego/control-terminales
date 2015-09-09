8cc0f55a6ec447ca266fb0e734635238b368ce17
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-09-09 12:13:23 -0300

    Agrego en models appointment la grabacion en el trigger de shipTrip, para guardar la diferencia de dias del alta al inicio y los días al T1 (esto depende el ships document que hay que implementarlo

--- models/appointment.js
+++ models/appointment.js
@@ -12,6 +12,11 @@ var appointment = new mongoose.Schema({
         fin: {type: Date},
         mov: {type: String, enum: ['IMPO', 'EXPO']},
         alta: {type: Date},
+        shipTrip: {
+            altaInicio: {type: Number},
+            shipT1: {type: Date},
+            altaShipT1: {type: Number}
+        },
         user: {type: String, uppercase: true},
         disponibles_t1: {type: Number},
         email: {type: String, lowercase: true},
@@ -33,5 +38,34 @@ appointment.statics.insert = function (appointment, cb) {
         });
     }
 }
+
+appointment.pre('save', function (next, done) {
+    var self = this;
+    var moment = require("moment"),
+        alta,
+        inicio,
+        shipT1,
+        Ships = require('./ship.js');
+
+    if (self.alta !== undefined) {
+        alta = moment(moment(self.alta).format("YYYY-MM-DD"), "YYYY-MM-DD");
+        inicio = moment(moment(self.inicio).format("YYYY-MM-DD"), "YYYY-MM-DD");
+        self.shipTrip.altaInicio = inicio.diff(alta, 'days');
+        Ships.aggregate([
+            {$match: {terminal: self.terminal, ship: self.buque}},
+            {$unwind: '$trips'},
+            {$match: {'trips.trip': self.viaje}}], function (err, shipTrips) {
+                if (!err) {
+                    if (shipTrips.length > 0) {
+                        shipT1 = moment(shipTrips[0].trips.date);
+                        shipT1 = shipT1.add(5, 'days');
+                        self.shipTrip.shipT1 = shipT1;
+                        self.shipTrip.altaShipT1 = shipT1.diff(alta, 'days');
+                    }
+                }
+        });
+    }
+    next();
+});
 
 module.exports = mongoose.model('appointments', appointment);
---
+++ models/ship.js
@@ -0,0 +1,16 @@
+/**
+ * Created by diego on 9/4/15.
+ */
+
+var mongoose = require('mongoose');
+
+var ship = new mongoose.Schema({
+    terminal: {type: String, required: true},
+    ship: {type: String},
+    trips: [{
+        trip: {type: String},
+        date: {type: Date}
+    }]
+});
+
+module.exports = mongoose.model('ship', ship);
