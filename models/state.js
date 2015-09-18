782506734f5608ee8364faf8890603b2bf760862
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-09-18 12:06:04 -0300

    Implementacion de metodo static asKeyValue

--- models/state.js
+++ models/state.js
@@ -2,13 +2,31 @@
  * Created by diego on 12/10/14.
  */
 
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
+    State;
 
-var states = new mongoose.Schema({
+State = new mongoose.Schema({
     _id: {type: String},
     name: {type: String, required: true},
     description: {type: String, required: true},
     type: {type: String, required: true, enum: ['ERROR', 'OK', 'WARN', 'UNKNOWN']}
 });
 
-module.exports = mongoose.model('states', states);
+State.statics.asKeyValue = function (callback) {
+    'use strict';
+    var state,
+        response = {};
+    state = this.find();
+    state.exec(function (err, states) {
+        if (err) {
+            callback(err);
+        } else {
+            states.forEach(function (item) {
+                response[item._id] = item;
+            });
+            callback(undefined, response);
+        }
+    });
+}
+
+module.exports = mongoose.model('states', State);
