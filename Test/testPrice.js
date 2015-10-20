724e7634ca89e73fbd40333ed08ef73233de0a2d
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-10-20 13:02:15 -0300

    Refactor en price lib. para que retorne el codigo y el precio

---
+++ Test/testPrice.js
@@ -0,0 +1,21 @@
+/**
+ * Created by diego on 10/13/15.
+ */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+mongoose.connect('mongodb://10.1.0.60/terapi', {
+    user: 'admin',
+    pass: 'Pt trend 54',
+    auth:{authdb:"admin"}
+});
+
+var price = require('../include/price.js');
+
+price = new price.price("BACTSSA");
+
+price.rates(true,  function (err, data) {
+    console.log(data);
+});
--- include/price.js
+++ include/price.js
@@ -9,10 +9,82 @@ var price = function (terminal) {
     this.matchPrice = require("../models/matchPrice.js");
     this.terminal = terminal;
 
+
+    this.ratesLocal = function (withDescription, callback) {
+        var Enumerable = require('linq'),
+            selfMatchPrice,
+            params = [],
+            self = this;
+
+        if (typeof withDescription === 'function') {
+            callback = withDescription;
+            withDescription = false;
+        }
+
+        if (callback !== undefined) {
+            selfMatchPrice =this.matchPrice;
+
+            if (self.terminal !== undefined) {
+                params.push({ $match: { terminal: self.terminal}});
+            } else {
+                params.push({ $match: { terminal: { $exists : 1}}});
+            }
+
+            params.push({ $project: {match: 1, price: 1}});
+            params.push({$unwind: '$match'});
+            selfMatchPrice.aggregate(params, function (err, data) {
+                selfMatchPrice.populate(data, [{ path: 'price', match: {rate: {$exists: 1}} }], function (err, matchprices) {
+                    var ratesDesc = {},
+                        result,
+                        a;
+
+                    if (err) {
+                        if (typeof callback === 'function')
+                            return callback(err);
+                    } else {
+                        result = Enumerable.from(matchprices)
+                            .where(function (item){
+                                return item.price != null;
+                            });
+
+                        if (withDescription === true) {
+                            a = result.select(function(item){
+                                ratesDesc[item.match] = item.price.description;
+                                return item;
+                            }).toArray();
+                            result = ratesDesc;
+                        } else if (withDescription === false) {
+                            result = result.select(function(item){
+                                return item.match;
+                            }).toArray();
+                        } else if (withDescription === 'todo') {
+                            a = result.select(function(item) {
+                                ratesDesc = {
+                                    code: item.match,
+                                    price: item.price
+                                };
+                                //ratesDesc = item.price;
+                                //ratesDesc.code = item.match;
+                                return ratesDesc;
+                            }).toArray();
+                            result = a;
+                        }
+                        if (typeof callback === 'function')
+                            return callback( undefined, result);
+                    }
+                });
+            });
+        }
+    }
 }
 
 price.prototype = {
 	rates: function (withDescription, callback) {
+
+        this.ratesLocal(withDescription, function (err, data) {
+            callback(err, data);
+        });
+/*
         var Enumerable = require('linq'),
             selfMatchPrice,
             params = [],
@@ -65,7 +137,9 @@ price.prototype = {
                     });
             });
         }
+        */
     }
+
 }
 
 exports.price = price;
