f084be03a3f090009a2a7a416cc55dacb957843e
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-10-20 13:03:23 -0300

    En vourcherTypes, para que retorne una abreviatura de la descripcion. "abbrev"

--- models/voucherType.js
+++ models/voucherType.js
@@ -7,6 +7,7 @@ var mongoose = require("mongoose");
 var voucher = new mongoose.Schema({
     _id: {type: Number},
     description: {type: String, required: true},
+    abbrev: {type: String},
     type: {type: Number, required: true, enum: [1, -1]}
 });
 
