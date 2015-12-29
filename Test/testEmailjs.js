5b738ff41c95da8d9729bf4968bbf7039fa9ae75
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-12-29 10:56:15 -0300

    Testing differents types of sending email

--- Test/testEmailjs.js
+++ Test/testEmailjs.js
@@ -1,17 +1,22 @@
 var mail = require("../include/emailjs");
 var config = require("../config/config.js");
+var fs = require('fs');
+
+//var privateKey = fs.readFileSync('exchange_new.pfx' );
 
 var options = {
     user:    "noreply",
     password: "desarrollo",
-    host:    "10.10.0.170",
-    port: "25",
-    domain: "puertobuenosaires.gob.ar",
+    host:    "10.10.0.176",
+    port: "587",
+//    domain: "puertobuenosaires.gob.ar",
+    domain: "agp",
     from: "Administración General de Puertos <noreply@puertobuenosaires.gob.ar>",
-    ssl:     false,
+    ssl: false,
     status: true,
     throughBcc: false
 }
+
 
 //var emailConfig = Object.create(config.email);
 //var emailConfig = Object.create(options);
@@ -19,7 +24,8 @@ var options = {
 console.log(options);
 mail = new mail.mail(options);
 
-mail.send("dreyes@puertobuenosaires.gob.ar", "hola", "mensaje", function (err, meba) {
+mail.send("reyesdiego@hotmail.com", "hola", "mensaje", function (err, meba) {
+//    mail.send("reyesdiego@hotmail.com", "hola", "mensaje", function (err, meba) {
     if (err) {
         console.error(err);
     } else {
--- Test/testNodeMailerGmail.js
+++ Test/testNodeMailerGmail.js
@@ -5,13 +5,31 @@
 var nodemailer = require('nodemailer');
 
 // create reusable transporter object using SMTP transport
+
+/*
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'Gmail',
     auth: {
-        user: 'reyesdiego3060@gmail.com',
-        pass: 'gcomputer'
+        user: 'reyesdiego@hotmail.com',
+        pass: ''
     }
 });
+*/
+
+
+var transporter = nodemailer.createTransport({
+    host: "10.10.0.176", // hostname
+    secureConnection: false, // use SSL
+    domain: "puertobuenosaires.gob.ar",
+    port: 25, // port for secure SMTP
+    auth: {
+        user: "",
+        pass: ""
+    }
+});
+
+
+
 
 // NB! No need to recreate the transporter object. You can use
 // the same transporter object for all e-mails
