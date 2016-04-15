79a5e188a1106919939fe0a709bf1448160b3f05
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2016-04-15 17:12:19 -0300

    Refactor de config file

---
+++ Test/testConfig.js
@@ -0,0 +1,6 @@
+/**
+ * Created by diego on 4/15/16.
+ */
+var config = require('../config/config.js');
+
+console.log(config.url);
--- config/config.js
+++ config/config.js
@@ -1,74 +1,12 @@
 /**
  * Created by Administrator on 1/17/14.
  */
-module.exports = {
-    /**
-     * Millis conversions cheat sheet:
-     * 1 second: 1000
-     * 1 minute: 60000
-     * 10 minutes: 600000
-     * 30 minutes: 1800000
-     * 1 hour: 3600000
-     * 12 hours: 43200000
-     * 24 hours: 86400000
-     * 1 week: 604800000
-     */
-    'ttl': 3600000, //1 hour
-    'resetTokenExpiresMinutes': 20, //20 minutes later
-    'processes': 0,
-    'url': "http://10.10.0.223:8080",
-    'socket_url': "http://10.10.0.223:8090",
-    'domain': "10.10.0.223",
-    'emailTurnos': {
-        user:    "",
-        password: "",
-        host:    "10.10.0.176",
-        port: "25",
-        domain: "",
-        from: "Administración General de Puertos <noreply@puertobuenosaires.gob.ar>",
-        ssl:     false,
-        status: true,
-        throughBcc: true
-    },
-    'email': {
-        user:    "",
-        password: "",
-        host:    "10.10.0.176",
-        port: "25",
-        domain: "",
-        from: "Administración General de Puertos <noreply@puertobuenosaires.gob.ar>",
-        ssl:     false,
-        status: true,
-        throughBcc: true
-    },
-    'log': {
-        path: 'log/',
-        filename: 'nohup.out',
-        toConsole: true,
-        toFile: false
-    },
-    'mongo_url': 'mongodb://10.1.0.60:27017/terapi',
-    'mongo_opts': {
-        user: 'admin',
-        pass: 'Pt trend 54',
-        auth: {authdb: "admin"},
-        server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }}
-    },
+var config;
 
-    //'mongo_url': 'mongodb://localhost:27017/terapi',
-    //'mongo_opts': {
-    //    user: 'admin',
-    //    pass: 'desarrollo',
-    //    auth: {authdb: "admin"},
-    //    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }}
-    //},
-    'mongo_url_log': 'mongodb://localhost:27017/logging',
-    'mongo_opts_log': {
-        user: 'admin',
-        pass: 'desarrollo',
-        auth: {authdb: "admin"}
-    },
-    'server_port_ter': 8080,
-    'server_port_web': 8090,
-    'server_ssl_port': 443
-};
+if (process.env.NODE_ENV === 'development') {
+    config = require('./config_dev.json');
+} else {
+    config = require('./config_pro.json');
+}
+
+module.exports = config;
