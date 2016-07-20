c2373bfb725cebd824fe7758c8e17feef286ff97
Diego Reyes <reyesdiego@hotmail.com>
2016-07-20 15:51:12 -0300

    Bug fix en insert invoice detail en oracle

--- Test/testOracleDbResultset.js
+++ Test/testOracleDbResultset.js
@@ -3,10 +3,10 @@
  */
 
 var oracledb = require('oracledb');
-//oracledb.maxRows = 103;
-//oracledb.outFormat = 2;
+//oracledb.maxRows = 100;
+//oracledb.outFormat = oracledb.OBJECT;
 
-var numRows = 10;  // number of rows to return from each call to getRows()
+var numRows = 1000;  // number of rows to return from each call to getRows()
 
 oracledb.getConnection(
     {
@@ -25,8 +25,22 @@ oracledb.getConnection(
             console.error(err.message);
             return;
         }
+        let strSql = "SELECT DISTINCT ID1.CONTENEDOR " +
+            "   FROM INVOICE_DETAIL ID1 " +
+            "       INNER JOIN INVOICE_HEADER IH1 ON ID1.INVOICE_HEADER_ID = IH1.ID " +
+            "   WHERE NOT EXISTS ( " +
+            "           SELECT * " +
+            "           FROM INVOICE_DETAIL INVD " +
+            "               INNER JOIN INVOICE_HEADER INVH ON INVD.INVOICE_HEADER_ID = INVH.ID " +
+            "               INNER JOIN TARIFARIO_TERMINAL TT ON TT.CODE = INVD.CODE AND TT.TERMINAL = INVH.TERMINAL " +
+            "               INNER JOIN TARIFARIO T ON T.ID = TT.TARIFARIO_ID " +
+            "           WHERE RATE is not null AND " +
+            "                   INVD.CONTENEDOR = ID1.CONTENEDOR AND " +
+            "                   INVH.TERMINAL = IH1.TERMINAL ) AND " +
+            "   IH1.TERMINAL = 'TRP' and " +
+            "   CONTENEDOR IS NOT NULL ";
         connection.execute(
-            "select * FROM COUNTRIES",
+            strSql,
             [],
             {
                 resultSet: true, // return a result set.  Default is false
@@ -40,15 +54,36 @@ oracledb.getConnection(
                     return;
                 }
             //    console.log(result);
-                fetchRowsFromRS(connection, result.resultSet, numRows);
+                //fetchRowsFromRS(connection, result.resultSet, numRows);
+                getResultSet(connection, result.resultSet, numRows)
+                .then(data => {
+                        "use strict";
+                        console.log(data);
+                    })
+                .catch(err => {
+                        "use strict";
+                       console.error(err);
+                    });
 //                console.log("metadata: %j", result.metaData);
   //              console.log("rows: %s, count: %j ", result.rows.length, result.rows);
     //            doRelease(connection);
             });
     });
 
-function fetchRowsFromRS(connection, resultSet, numRows)
-{
+function getResultSet (connection, resultSet, numRows) {
+    return new Promise((resolve, reject) => {
+        var ret = [];
+        fetchRowsFromRS(connection, resultSet, numRows, ret, (err, ret) => {
+            "use strict";
+            if (err) {
+                reject(err);
+            } else {
+                resolve(ret.length);
+            }
+        });
+    });
+}
+function fetchRowsFromRS(connection, resultSet, numRows, ret, callback) {
     resultSet.getRows( // get numRows rows
         numRows,
         function (err, rows)
@@ -56,12 +91,16 @@ function fetchRowsFromRS(connection, resultSet, numRows)
             if (err) {
                 console.log(err);
                 doClose(connection, resultSet); // always close the result set
+                callback(err);
             } else if (rows.length === 0) {    // no rows, or no more rows
                 doClose(connection, resultSet); // always close the result set
+                callback(undefined, ret);
             } else if (rows.length > 0) {
-                console.log("fetchRowsFromRS(): Got " + rows.length + " rows");
-                console.log(rows);
-                fetchRowsFromRS(connection, resultSet, numRows);
+                rows.map(item => {
+                    "use strict";
+                    ret.push(item[0]);
+                });
+                fetchRowsFromRS(connection, resultSet, numRows, ret, callback);
             }
         });
 }
