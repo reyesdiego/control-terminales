8c58208a1a767d1b2330ac95b6286b34fb3988c7
Diego Reyes <reyesdiego@hotmail.com>
2017-07-28 12:11:30 -0300

    Refactor, patente validacion mercosur

--- include/patente.js
+++ include/patente.js
@@ -7,11 +7,28 @@ let patente = (patente) => {
 
     patente = (patente || "").toUpperCase();
 
-    var patronViejo = /^[A-Z]{3}[0-9]{3}$/;
-    var patronNuevo = /^[A-Z]{2}[0-9]{3}[A-Z]{2}$/;
+    /** Mercosur */
+    var AR = /^[A-Z]{2}[0-9]{3}[A-Z]{2}$/;
+    var BR = /^[A-Z]{2}[0-9]{3}[A-Z]{2}$/;
+    var VE = /^[A-Z]{2}[0-9]{3}[A-Z]{2}$/;
+    var UR = /^[A-Z]{3}[0-9]{4}$/;
+    var PY = /^[0-9]{3}[A-Z]{4}$/;
 
-    return patronViejo.test(patente) || patronNuevo.test(patente);
+    /** Argentina */
+    var AR_OLD = /^[A-Z]{3}[0-9]{3}$/;
+    /** Paraguay */
+    var PY_OLD = /^[A-Z]{3}[0-9]{3}$/;
+    /** Brasil */
+    var BR_OLD = /^[A-Z]{3}[0-9]{4}$/;
 
+    return AR.test(patente) ||
+            BR.test(patente) ||
+            VE.test(patente) ||
+            UR.test(patente) ||
+            PY.test(patente) ||
+            AR_OLD.test(patente) ||
+            PY_OLD.test(patente) ||
+            BR_OLD.test(patente);
 };
 
 module.exports.check = patente;
