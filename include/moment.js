6c7e24405c5842e5ad689fb219b50d328f67fb0b
Diego Reyes <reyesdiego@hotmail.com>
2017-10-19 16:11:24 -0300

    Refactor, datos en gates del movimiento de entrada y salida y tiempo de espera

--- include/moment.js
+++ include/moment.js
@@ -1,40 +1,65 @@
 /**
  * Created by diego on 4/3/14.
  */
+"use strict";
 
-function dateTime() {
-	var moment = require('moment');
-	return moment().format('YY-MM-DD HH:mm');
-}
+var moment = require('moment');
 
-function objectIdNow() {
-	var moment = require('moment');
+let dateTime = () => {
+    return moment().format('YY-MM-DD HH:mm');
+};
 
-		date = moment();
+let objectIdNow = () => {
+    return Math.floor(moment().getTime() / 1000).toString(16) + "0000000000000000";
+};
 
-	var obj = Math.floor(date.getTime() / 1000).toString(16) + "0000000000000000";
-	return obj;
-}
+let objectId0000 = fecha => {
 
-function objectId0000(fecha) {
-	var moment = require('moment');
+    var date;
+    if (fecha !== undefined) {
+        date = moment(fecha);
+    } else {
+        date = moment();
+    }
 
-	var date;
-	if (fecha !== undefined)
-		date = moment(fecha);
-	else
-		date = moment();
+    return Math.floor(new Date(date.year(),date.month(), date.date()) / 1000).toString(16) + "0000000000000000";
+};
 
-	var obj = Math.floor(new Date(date.year(),date.month(), date.date()) / 1000).toString(16) + "0000000000000000";
-	return obj;
-}
-
-function dateTimeFromObjectId (guid) {
+let dateTimeFromObjectId = guid => {
 	return new Date(parseInt(guid.toString().slice(0,8), 16)*1000);
-}
+};
 
+let durationPeriod = (dateIni, dateEnd) => {
+
+    var dif = moment(dateEnd).diff(moment(dateIni));
+    return this.durationMillisecond(dif);
+};
+
+let durationMillisecond = (ms) => {
+
+    var d = moment.duration(ms, 'milliseconds');
+    var days = Math.floor(d.asDays());
+    var hours = Math.floor(d.asHours());
+    var mins = ("00" + (Math.floor(d.asMinutes()) - hours * 60).toString());
+    mins = mins.substr(mins.length - 2, 2);
+    var result;
+
+    if (days > 0) {
+        hours = hours - (days * 24);
+        result = days + " días, " + hours + ":" + mins + " hs.";
+    } else {
+        if (hours > 0) {
+            result = hours + ":" + mins + " hs.";
+        } else {
+            result = parseInt(mins).toString() + " min.";
+        }
+    }
+    return result;
+};
 
 module.exports.getDatetime = dateTime;
 module.exports.getObjectIdNow = objectIdNow;
 module.exports.getObjectId0000 = objectId0000;
 module.exports.getDateTimeFromObjectId = dateTimeFromObjectId;
+module.exports.durationPeriod = durationPeriod;
+module.exports.durationMillisecond = durationMillisecond;
