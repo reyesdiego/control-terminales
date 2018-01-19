50d3a53fb10e4c311a9bb1d5b476b26ada4440ff
Diego Reyes <reyesdiego@hotmail.com>
2018-01-19 11:35:32 -0300

    Cambia directorio

--- log.sh
+++ log.sh
@@ -2,6 +2,6 @@
 DATE=$(date +"%m-%d-%Y")
 NOW=$(date +"%Y-%m-%dT%H:%M:%S")
 FILE="nohup.$DATE.out"
-echo "{ \"timestamp\":\"$NOW\", \"level\":\"info\",\"message\":\"======== continue on $FILE ==========\"}" >> /home/diego/agp/agpapi/log/nohup.out
-sudo cp /home/diego/agp/agpapi/log/nohup.out /home/diego/agp/agpapi/log/$FILE
-echo "{ \"timestamp\":\"$NOW\", \"level\":\"info\",\"message\":\"======== comes from $FILE ==========\"}" > /home/diego/agp/agpapi/log/nohup.out
+echo "{ \"timestamp\":\"$NOW\", \"level\":\"info\",\"message\":\"======== continue on $FILE ==========\"}" >> /home/desarrollo/control-terminales/log/nohup.out
+sudo cp /home/desarrollo/control-terminales/log/nohup.out /home/desarrollo/control-terminales/log/$FILE
+echo "{ \"timestamp\":\"$NOW\", \"level\":\"info\",\"message\":\"======== comes from $FILE ==========\"}" > /home/desarrollo/control-terminales/log/nohup.out
