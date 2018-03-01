1bf5dc698e39149600433ac20b51ad49fcefe1b3
Diego Reyes <reyesdiego@hotmail.com>
2018-03-01 12:03:10 -0300

    Tyding code, minor change en uncaughtException

--- mainTer.js
+++ mainTer.js
@@ -4,95 +4,93 @@
 
 "use strict";
 
-var config = require('./config/config.js'),
-    log4n = require('./include/log/log4node.js'),
+var config = require("./config/config.js"),
+    log4n = require("./include/log/log4node.js"),
     log = new log4n.log(config.log),
-    io = require('socket.io-client'),
+    io = require("socket.io-client"),
     ioClient,
     params,
-    moment = require('moment'),
+    moment = require("moment"),
     port = process.env.PORT || config.server_port_ter,
     httpExpress,
     oracle;
 
 //moment.locale('es');
 /** Conecta a la base de datos MongoDb */
-require('./include/mongoose.js')(config.mongo.url, config.mongo.options, log);
+require("./include/mongoose.js")(config.mongo.url, config.mongo.options, log);
 
 /** Crea un servidor http en puerto 8080 */
-httpExpress = require('./include/httpExpress.js')(log, port, true);
+httpExpress = require("./include/httpExpress.js")(log, port, true);
 
 /** Se conecta al servidor Socket */
-ioClient = io.connect(config.socket_url, { 'forceNew': false});
-ioClient.once('connect', function () {
+ioClient = io.connect(config.socket_url, { "forceNew": false });
+ioClient.once("connect", function () {
     log.logger.info("Conectado al socket IO %s", config.socket_url);
 });
-ioClient.on('reconnect', function() {
+ioClient.on("reconnect", function () {
     log.logger.info("RE Conectado al socket IO %s", config.socket_url);
 });
-ioClient.once('connect_error', function() {
+ioClient.once("connect_error", function () {
     log.logger.error("Error Conectando al socket IO %s", config.socket_url);
 });
 
 /** Conexion a ORACLE */
-oracle = require('./include/oracle.js');
+oracle = require("./include/oracle.js");
 oracle = new oracle();
 //oracle.oracledb.maxRows = 5000;
 oracle.oracledb.createPool({
     user: "afip",
     password: "AFIP_",
     connectString: "(DESCRIPTION = " +
-                    "(ADDRESS = (PROTOCOL = TCP)(HOST = 10.1.0.60)(PORT = 1521)) " +
-                    "(CONNECT_DATA = " +
-                    "        (SID = AFIP) " +
-                    ") " +
-                    ")",
+        "(ADDRESS = (PROTOCOL = TCP)(HOST = 10.1.0.60)(PORT = 1521)) " +
+        "(CONNECT_DATA = " +
+        "        (SID = AFIP) " +
+        ") " +
+        ")",
     poolMax: 500,
     poolMin: 2,
     poolIncrement: 5,
     poolTimeout: 4
 }, //TODO check ORA-24418
-    (err, pool) => {
+(err, pool) => {
 
-        oracle.pool = pool;
-        log.logger.info("Oracle Connected to Database. Versión %s", oracle.oracledb.oracleClientVersion);
+    oracle.pool = pool;
+    log.logger.info("Oracle Connected to Database. Versión %s", oracle.oracledb.oracleClientVersion);
 
-        if (err) {
-            log.logger.error('Oracle: %s', err.message);
+    if (err) {
+        log.logger.error("Oracle: %s", err.message);
+    }
+
+    global.cache = {};
+
+    params = {
+        server: { ip: config.domain, port: port },
+        node: {
+            version: process.version,
+            runtime: httpExpress.app.get("runtime"),
+            timeElapsed: moment(moment(httpExpress.app.get("runtime"))).fromNow(true)
+        },
+        oracle: {
+            pool: pool
         }
+    };
 
-        global.cache = {};
+    var VoucherType = require("./lib/voucherType.js");
+    const voucherType = new VoucherType(oracle);
 
-        params = {
-            server: {ip: config.domain, port: port},
-            node: {
-                version: process.version,
-                runtime: httpExpress.app.get('runtime'),
-                timeElapsed: moment(moment(httpExpress.app.get('runtime'))).fromNow(true)
-            },
-            oracle: {
-                pool: pool
-            }
-        };
+    voucherType.getAll({}, { format: "array" })
+        .then(data => {
+            global.cache.voucherTypes = data.data;
+            require("./routes/routesTer")(log, httpExpress.app, ioClient, oracle, params);
+        })
+        .catch(err => {
+            log.logger.error(err);
+        });
 
-        var VoucherType = require('./lib/voucherType.js');
-        VoucherType = new VoucherType(oracle);
-
-        VoucherType.getAll({},{format: 'array'})
-            .then(data => {
-                global.cache.voucherTypes = data.data;
-                require('./routes/routesTer')(log, httpExpress.app, ioClient, oracle, params);
-            })
-            .catch(err => {
-                log.logger.error(err);
-            });
-
-    });
-
-process.on('exit', () => {
-    log.logger.error('exiting');
 });
 
-process.on('uncaughtException', function (err) {
-    log.logger.info("Caught exception: " + err);
+process.on("exit", () => log.logger.error("exiting..."));
+
+process.on("uncaughtException", err => {
+    log.logger.error("Uncaught Exception: " + err);
 });
